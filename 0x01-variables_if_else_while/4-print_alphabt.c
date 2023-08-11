b466bc76168d30f9fba33917f3874ab1f37dcc19
piliswalehe <swalehe_pili@yahoo.com>
2023-08-11 16:50:03 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet a++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		if (alphabet != 'q' && alphabet != 'e')
 		{
 		putchar(alphabet);
-		alphabet++
+		alphabet++;
 		}
 	putchar('\n');
 	return (0);
