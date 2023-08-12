c34f720e81289509e37445888523f323ee9e4ea4
piliswalehe <swalehe_pili@yahoo.com>
2023-08-11 18:18:07 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@ int main(void)
 	int alphabets = '0';
 
 	while
-		(alphabets <= '9');
-	{
-		putchar(alphabets);
-		alphabets++;
-	}
+		(alphabets <= '9')
+		{
+			putchar(alphabets);
+			alphabets++;
+		}
 	putchar('\n');
 	return (0);
 }
