df3b71f62fab705c7b7aca2f31be6f1e3914068f
piliswalehe <swalehe_pili@yahoo.com>
2023-08-17 16:48:50 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,16 @@
  */
 void print_most_numbers(void)
 {
-	int a = '0';
+	int a;
 
-	while (a <= 9)
+	for (a = 0; a <= 9; a++)
 	{
-		if (a != 2 && a != 4)
+		if (a == 2 && a == 4)
 		{
 			continue;
 		}
 		else
-			_putchar(a = '0');
+			_putchar(a + '0');
 	}
 	_putchar('\n');
 }
