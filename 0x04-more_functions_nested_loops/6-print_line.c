e2487f77029c4d649a9dc18469373eb10dff3386
piliswalehe <swalehe_pili@yahoo.com>
2023-08-17 17:32:22 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line to a given lenght.
+ * @n: length of the line.
+ *
+ *Returns: 0 if n is less 
+ */
+void print_line(int n)
+{
+	while (!(n <= 0))
+	{
+		_putchar('_');
+		n--;
+}
+	_putchar('\n');
+}
+
