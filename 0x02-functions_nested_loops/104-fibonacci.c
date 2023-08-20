4091c179a6a708b265ea679f64f5c972ef989f6b
piliswalehe <swalehe_pili@yahoo.com>
2023-08-20 11:18:47 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,27 +6,45 @@
  */
 int main(void)
 {
-	int count = 0;
-	long int a = 1, b = 2;
-	long int p = a + b;
+	int count;
+	unsigned long a = 0, b = 1, sum;
+	unsigned long a_half1, a_half2, b_half1, b_half2;
+	unsigned long half1, half2;
 
-	printf("%lu, ", a);
-	printf("%lu, ", b);
-
-	while (count < 89)
+	for (count = 0; count < 92; count++)
 	{
-		p = a + b;
-		if (count == 89)
-		{
-			printf("%lu\n", p);
-		}
-		else
-		{
-			printf("%lu, ", p);
-		}
+		sum = a + b;
+		printf("%lu, ", sum);
+
 		a = b;
-		b = p;
-		count++;
+		b = sum
 	}
+
+	a_half1 = a / 10000000000;
+	b_half1 = b / 10000000000;
+	a_half2 = a % 10000000000;
+	b_half2 = b % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		half1 = a_half1 + b_half1;
+		half2 = a_half2 + b_half2;
+		if (a_half2 + b_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+
+		a_half1 = b_half1;
+		a_half2 = b_half2;
+		a_half1 = half1;
+		b_half2 = half2;
+	}
+	printf("\n");
 	return (0);
 }
+
