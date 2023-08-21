9733db173e1ece93fc224b352b8ed44bf965cdc4
piliswalehe <swalehe_pili@yahoo.com>
2023-08-21 12:11:25 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that prints the value of an int
+ * @n: value pointer for deferencing
+ * Return: nth value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
