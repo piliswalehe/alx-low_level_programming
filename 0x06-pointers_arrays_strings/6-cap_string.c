582c9f59749a5766edfa2dfbc378e0881b0f2424
piliswalehe <swalehe_pili@yahoo.com>
2023-08-24 14:59:44 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,10 +9,11 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
+
 		if ((str[i - 1] == ' ') ||
 		(str[i - 1] == '\t') ||
 		(str[i - 1] == '\n') ||
@@ -26,9 +27,11 @@ char *cap_string(char *str)
 		(str[i - 1] == ')') ||
 		(str[i - 1] == '{') ||
 		(str[i - 1] == '}') ||
-		(str[i - 1] == '0'))
+		i == '0')
 			str[i] -= 32;
+
 		i++;
 	}
+
 	return (str);
 }
