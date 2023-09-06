c1e2165af48dd80f1654de65cf86b9eab77a7640
piliswalehe <swalehe_pili@yahoo.com>
2023-09-06 03:35:40 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,12 @@
 /**
  * main - prints its name, followed by a new line
  * @argc: number of line command arguments
- * @agrv: array with contains the program command line arguments
+ * @argv: array with contains the program command line arguments
  * Return: Always 0 success
  */
 int main(int argc__attribute__((unused)), char *argv)
 {
-	printf("./mynewnameis\n", argv[0]);
+	printf("%s\n", *argv);
+
 	return (0);
 }
