e8ed1445a28d5cccdfc61270643a0a9ed014af7d
piliswalehe <swalehe_pili@yahoo.com>
2023-09-11 16:28:13 -0700

    Tasks Done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,11 +14,11 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strncpy(char *dest, char *src, int n);
-int _strlen(char *s)
 
 #endif
