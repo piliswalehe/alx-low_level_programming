bb16a33695c045ae43e9609271066b0c766a1f54
piliswalehe <swalehe_pili@yahoo.com>
2023-09-13 11:47:04 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - function operator
+ * @a: input value
+ * @b: input value
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function operator
+ * @a: input value
+ * @b: input value
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function operator
+ * @a: input value
+ * @b: input value
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function operator
+ * @a: input value
+ * @b: input value
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - function operator
+ * @a: input value
+ * @b: input value
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a + b);
+}
