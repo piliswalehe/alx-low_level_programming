d4f6fbe69d9b763a627b9a7d46c26b67827a1f75
piliswalehe <swalehe_pili@yahoo.com>
2023-09-21 12:30:12 -0700

    Commits by Pili Swalehe

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,15 @@
 #include "lists.h"
-
 /**
- * print_list - prints all the elements of lists_t list
- * @h: singly linked list
- * Return: number of nodes
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: The number of nodes.
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t elem;
 
+	elem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -16,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
+		elem++;
 	}
-	return (count);
+	return (elem);
 }
