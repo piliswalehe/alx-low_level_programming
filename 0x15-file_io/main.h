43b0e34e102823de7c795acdde87cd81030c8946
piliswalehe <138812237+piliswalehe@users.noreply.github.com>
2023-10-30 22:20:19 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,8 +2,8 @@
 #define _MAIN_H_
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
