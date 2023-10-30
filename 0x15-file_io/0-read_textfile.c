79bbcefc814a328bef3cea88422c44611d33ad97
piliswalehe <138812237+piliswalehe@users.noreply.github.com>
2023-10-30 22:10:35 +0300

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: actual numberof letters it could print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  
