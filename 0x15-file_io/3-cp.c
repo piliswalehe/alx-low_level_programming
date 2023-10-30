8af1e0ffde5ece64ae7a1ce70070a4428d10eff1
pili swalehe <swalehe_pili@yahoo.com>
2023-10-30 16:30:06 -0700

    task 3"

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,11 @@
-#define _GNU_SOURCE
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 
-/** close_errorcheck - closes a file descriptor and prints an error message
+/**close__errorcheck - closes a file descriptor and prints an error message
  *
  * @file_descriptor: file descriptor to close
  *
