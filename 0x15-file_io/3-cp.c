acbd616473bbfbdc8a8a8e20930caa57c1974b64
piliswalehe <138812237+piliswalehe@users.noreply.github.com>
2023-10-31 13:52:34 +0300

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,15 +7,15 @@
 
 /**close__errorcheck - closes a file descriptor and prints an error message
  *
- * @file_descriptor: file descriptor to close
+ * @fd: file descriptor to close
  *
  * Return: 0 on success, -1 on failure
  */
-int close_errorcheck(int f_d)
+int close_errorcheck(int fd)
 {
 	int error;
 
-	error = close(f_d);
+	error = close(fd);
 	if (error == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
@@ -50,7 +50,7 @@ int write_error(int f_d1, int f_d2, char *filename)
  *
  * Return: 98
  */
-int read_error(int fd1, int fd2, char *filename)
+int read_error(int f_d1, int f_d2, char *filename)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 	close_errorcheck(f_d1);
@@ -74,36 +74,36 @@ int read_error(int fd1, int fd2, char *filename)
 int main(int ac, char *av[])
 {
 	char buff[1024];
-	int lenroad, lenwidth, from_file, to_file, error;
+	int lenroad, lenwidth, from_file, file_to, error;
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
-	file_from = open(av[1], O_RDONLY);
+	from_file = open(av[1], O_RDONLY);
 	if (from_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			av[1]);
 		return (98);
 	}
-	to_file = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
 		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (to_file == -1)
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close_errorcheck(from_file);
+		close_errorcheck(file_from);
 		return (99);
 	}
 	do 
 	{
-		lenroad = read(from_file, buff 1024);
+		lenroad = read(file_from, buff 1024);
 		if (lenroad == -1)
-			return (read_error(from_file, to_file, av[1]));
-		lenwidth = write(to_file, buff, lenroad);
+			return (read_error(file_from, file_to, av[1]));
+		lenwidth = write(file_to, buff, lenroad);
 		if (lenwidth == -1 || lenwidth != lenroad)
-			return (write_error(from_file, to_file, av[2]));
+			return (write_error(file_from, file_to, av[2]));
 	}
 	while (lenroad == 1024);
 	error = close_errorcheck(file_from);
