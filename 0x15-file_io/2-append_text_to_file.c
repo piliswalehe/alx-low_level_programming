96854ff5f1973f606de4bfa90ee9c78cd0dd91bb
piliswalehe <138812237+piliswalehe@users.noreply.github.com>
2023-11-03 23:45:24 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,6 +14,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		return(1);
 
 	nfile = open(filename, O_WRONLY | O_APPEND);
 	if (nfile == -1)
