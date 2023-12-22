35fe09575812e05d3631f9ae2610988ec3cfe95d
piliswalehe <swalehe_pili@yahoo.com>
2023-12-22 07:18:20 -0800

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return(NULL);
-	array = malloc(sizeof(hash_node_t) * size);
+	
+	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
 		return(NULL);
 
