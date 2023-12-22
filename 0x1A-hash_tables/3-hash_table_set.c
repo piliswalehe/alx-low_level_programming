0bf0ef9887e226eff77280e451ce75d87cc2570b
piliswalehe <swalehe_pili@yahoo.com>
2023-12-22 09:45:39 -0800

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -51,7 +51,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int my_index;
 
-	if (ht == NULL || key == NULL || *key != '\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	my_index = key_index((unsigned char *)key, ht->size);
